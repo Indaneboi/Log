8895d4d228449ccca5a83f7928fc43523d4f4062
Indaneboi <56091800+Indaneboi@users.noreply.github.com>
2021-07-30 02:23:05 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
-import { faHeart} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faReact } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
   return (
     <div className="footer">
       <p>
-        Made with <FontAwesomeIcon color="#ADAFBC" icon={faHeart} /> by Anirudh Yadav
+        Made with <FontAwesomeIcon color="#b7bbc8" icon={faReact} /> by Anirudh Yadav
       </p>
     </div>
   );
