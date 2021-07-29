5b090b73c3a00c72e95cec2e91fba3df757b05cc
Indaneboi <56091800+Indaneboi@users.noreply.github.com>
2021-07-30 02:17:13 +0530

    Add files via upload

---
+++ build/precache-manifest.5e38f435b9658fb19ee13053c3a192ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b360b728a55702e0859843f423b12d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1178aa07adc1f5917c05",
+    "url": "/static/css/main.eef31983.chunk.css"
+  },
+  {
+    "revision": "28957cd8a97c6e7b15a9",
+    "url": "/static/js/2.42bb56e5.chunk.js"
+  },
+  {
+    "revision": "1178aa07adc1f5917c05",
+    "url": "/static/js/main.e08c77c6.chunk.js"
+  },
+  {
+    "revision": "1078bbc7f8c94a05ce0d",
+    "url": "/static/js/runtime-main.1aa32bfa.js"
+  }
+]);
